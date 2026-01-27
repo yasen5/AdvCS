85900ee587c87e05c1a21f394cc2a8fd35395070
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-01-27 14:47:57 -0800

    Add more features

--- Jan15/Runner.java
+++ Jan15/Runner.java
@@ -10,6 +10,10 @@ public class Runner extends JPanel implements ActionListener {
     private BinarySearchTree<Integer> tree;
     private JTextField input;
     private JButton addButton;
+    private JButton removeButton;
+    private JButton clearButton;
+    private JButton containsButton;
+    private boolean containsResult = false;
 
     public Runner() {
         tree = new BinarySearchTree<>();
@@ -32,31 +36,50 @@ public class Runner extends JPanel implements ActionListener {
         tree.add(67);
         tree.add(82);
 
-
         input = new JTextField(5);
         addButton = new JButton("Add");
         addButton.addActionListener(this);
 
+        removeButton = new JButton("Remove");
+        removeButton.addActionListener(this);
+
+        clearButton = new JButton("Clear");
+        clearButton.addActionListener(this);
+
+        containsButton = new JButton("Contains");
+        containsButton.addActionListener(this);
+
         this.add(new JLabel("Enter Value:"));
         this.add(input);
         this.add(addButton);
+        this.add(removeButton);
+        this.add(clearButton);
+        this.add(containsButton);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         tree.drawMe(g);
+        if (containsResult) {
+            g.drawString("Contained!", 50, 50);
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
-        try {
+        if (e.getSource() == addButton) {
             int value = Integer.parseInt(input.getText());
             tree.add(value);
             input.setText("");
-            repaint();
+        } else if (e.getSource() == removeButton) {
+            int value = Integer.parseInt(input.getText());
+            tree.remove(value);
+            input.setText("");
+        } else if (e.getSource() == clearButton) {
+            tree.clear();
+        } else if (e.getSource() == containsButton) {
+            containsResult = tree.contains(Integer.parseInt(input.getText()));
         }
-        catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(this, "Please enter a valid integer.");
-        }
+        repaint();
     }
 
     public static void main(String[] args) {
